feceff40a5f46481ba32f10551e61c97b97bb863
Kees Sonnema <info@keessonnema.nl>
2017-08-04 21:57:50 +0200

    Add Youtube API & Spotify API, make the search work

--- kezbot.py
+++ kezbot.py
@@ -4,19 +4,72 @@
 import subprocess
 import configparser
 import requests
+import pprint
 import strings
 import os
 import time
 import sys
 import logging
 import spotipy
+import spotipy.util as util
+import urllib.request, urllib.error
+import simplejson
+import re
+import requests
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from config import Config
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+
+# spotipy stuff
+api = Config.YOUTUBE_API_KEY
 
 
-#basic commands for the bot
+def getify(bot, update, args):
+    getify_link = args[0]
+    pattern = r'(?:https?:\/\/)?(?:[0-9A-Z-]+\.)?(?:youtube|youtu|youtube-nocookie)\.(?:com|be)\/(?:watch\?v=|watch\?.+&v=|embed\/|v\/|.+\?v=)?([^&=\n%\?]{11})'
+    result = ' '.join(re.findall(pattern, getify_link, re.MULTILINE | re.IGNORECASE))
+
+    print(result)
+
+    id = result
+    url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id={0}&key={1}".format(id, api)
+
+    print(url)
+    json = simplejson.load(urllib.request.urlopen(url))
+
+    title = json['items'][0]['snippet']['title']
+    result = re.sub("[\(\[].*?[\)\]]", "", title)
+    update.effective_message.reply_text("You've searched for: "
+                                        "{0}. "
+                                        "Let me find it on Spotify!" .format(result))
+
+    client_id = 'cdd44100bf3c4d9fa383721521bec613'
+    client_secret = '1b3e855446e64739a4950b894b237b4d'
+    redirect_uri = 'http://localhost:8000/callback/'
+
+    os.environ["SPOTIPY_CLIENT_ID"] = client_id
+    os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
+    os.environ["SPOTIPY_REDIRECT_URI"] = redirect_uri
+
+    username = 'keessonnema'
+    scope = 'user-read-private'
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.search(q=result, type='track', limit=1)
+        pp = pprint.PrettyPrinter(indent=4)
+#        pp.pprint(results)
+#        quit()
+        if results:
+            spotitle = results['tracks']['items'][0]['name']
+            spoturl = results['tracks']['items'][0]['external_urls']['spotify']
+            update.effective_message.reply_text("{0} {1}".format(spotitle, spoturl))
+        else:
+            update.effective_message.reply_text("I can't find this track on Spotify :(")
+    else:
+        print("There's something wrong with the token")
+
+# basic bot commands
 
 owner_id = int(Config.OWNER_ID)
 
@@ -58,9 +111,13 @@ def getid(bot, update):
 
 
 def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
     token = Config.API_KEY
     updater = Updater(token)
+
+    updater.dispatcher.add_handler(CommandHandler("getify", getify, pass_args=True))
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
