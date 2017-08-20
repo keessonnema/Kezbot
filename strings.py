b928608f5eb6c177dc4f86a017a098a703ad4772
Kees Sonnema <info@keessonnema.nl>
2017-08-20 14:44:32 +0200

    Remove some strings that limit search results

--- strings.py
+++ strings.py
@@ -1,9 +1,9 @@
 MatchPattern = '([a-z]+?:\/\/)*([a-z]*?[.])*youtu([.]be|be[.][a-z]+?)\/((watch[?]v=|v)*).+'
 YoutubePattern = r'(?:https?:\/\/)?(?:[0-9A-Z-]+\.)?(?:youtube|youtu|youtube-nocookie)\.' \
                   r'(?:com|be)\/(?:watch\?v=|watch\?.+&v=|embed\/|v\/|.+\?v=)?([^&=\n%\?]{11})'
-RemoveWords = "\\b(ft|feat|HQ|HD|4k|lyrics|lyric|1080p)\\b"
+RemoveWords = "\\b(ft|feat)\\b"
 KeepWords = ["remix", "edit"]
-StringRegex = r'\[[^\]]*\]|\(\d+\)|“.*?”|".*?"|[.]|[&]|[,]|(#[A-Za-z0-9]+)'
+StringRegex = r'\[[^\]]*\]|\(\d+\)|“.*?”|".*?"|[.]|[&]|[,]'
 
 run_strings = (
     "Where do you think you're going?",
