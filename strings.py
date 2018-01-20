f1edc86ab6d57185a17685ae0f7c9d68f8289c35
Kees Sonnema <info@keessonnema.nl>
2018-01-20 15:18:32 +0100

    Remove unused strings

--- strings.py
+++ strings.py
@@ -21,17 +21,13 @@ run_strings = (
     "Run fatboy, run!",
     "Jokes on you, I'm everywhere",
     "You're gonna regret that...",
-    "You could also try /kickme, I hear thats fun.",
     "Go bother someone else, no-one here cares.",
     "I hear @MSFJarvis wants to hear more about you.",
-    "If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. \
-    But if you don't, I will look for you, I will find you, and I will kill you.",
+    "If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.",
     "I drink your milkshake!",
-    "Ahoy m8",
     "Go ahead, make my day.",
     "Hasta la vista, baby.",
     "I'll get you, my pretty, and your little dog, too!",
     "You can't handle the truth!",
-    "Frankly, my dear, I don't give a damn.",
-    ""
+    "Frankly, my dear, I don't give a damn."
 )
